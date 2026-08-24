df9a0d36bd39f7ea346f7e9673d3d9edaf8bc90c
MCF <143879270+mo-yjbbb@users.noreply.github.com>
2026-08-24 19:30:34 +0800

    Fix repository path for Core/StillPhotoProcessor.swift

---
+++ Core/StillPhotoProcessor.swift
@@ -0,0 +1,24 @@
+import Photos
+import CoreImage
+import UIKit
+
+enum StillPhotoProcessor {
+    static func process(asset: PHAsset) async throws {
+        let input = try await asset.contentEditingInput()
+        guard let url = input.fullSizeImageURL,
+              let ciImage = CIImage(contentsOf: url, options: [.applyOrientationProperty: true]) else {
+            throw WatermarkError.assetUnavailable
+        }
+        let renderer = GlassRenderer(metadata: MetadataReader.read(asset: asset, input: input))
+        guard let cgImage = renderer.makeCGImage(renderer.render(ciImage)) else { throw WatermarkError.renderFailed }
+        let data = UIImage(cgImage: cgImage).jpegData(compressionQuality: 0.96)
+        guard let data else { throw WatermarkError.renderFailed }
+        try await PHPhotoLibrary.shared().performChanges {
+            let request = PHAssetCreationRequest.forAsset()
+            request.addResource(with: .photo, data: data, options: nil)
+            request.creationDate = asset.creationDate
+            request.location = asset.location
+        }
+    }
+}
+
