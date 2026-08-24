a45c478151816bc4510538afc5201a028662f3b5
MCF <143879270+mo-yjbbb@users.noreply.github.com>
2026-08-24 20:38:01 +0800

    Rebuild liquid glass renderer and harden Live Photo processing

--- Core/PhotoInput.swift
+++ Core/PhotoInput.swift
@@ -5,6 +5,7 @@ extension PHAsset {
         try await withCheckedThrowingContinuation { continuation in
             let options = PHContentEditingInputRequestOptions()
             options.isNetworkAccessAllowed = true
+            options.canHandleAdjustmentData = { _ in false }
             requestContentEditingInput(with: options) { input, _ in
                 if let input { continuation.resume(returning: input) }
                 else { continuation.resume(throwing: WatermarkError.assetUnavailable) }
